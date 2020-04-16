c1dcff7dee41d04006ec0d69946afc9ad298e74b
taku1508 <60663610+taku1508@users.noreply.github.com>
2020-04-16 18:18:43 +0900

    s3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,17 @@ class ApplicationController < ActionController::Base
   def configre_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
+  
+  before_action :basic_auth
 
+  private
+  
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials[:basic_auth][:user] &&
+      password == Rails.application.credentials[:basic_auth][:pass]
+    end
+  end
 
 end
+
